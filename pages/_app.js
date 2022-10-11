d2d935ab632215902fe7c2cbd27938023856c49d
Imam Nur Arifin <imam.12ra.kkpi@gmail.com>
2022-10-11 21:54:52 +0700

    section header

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,26 @@
-import '../styles/globals.css'
+import { ThemeProvider } from '@mui/material/styles';
+import Head from 'next/head';
+import CssBaseline from '@mui/material/CssBaseline';
+import theme from '../utils/theme';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Netflix-MUI</title>
+        <meta
+          name="viewport"
+          content="width=device-width, height=device-height, initial-scale=1.0, user-scalable=yes"
+        />
+        <link rel="shortcut icon" href="/nficon2016.ico" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
